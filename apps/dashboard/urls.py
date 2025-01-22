9946e6af2cffc6555b779e10c3a2a1c439d916ff
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2025-01-22 12:16:38 +0530

    auth done

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -5,9 +5,11 @@ from apps.dashboard.views import AuthViews, DashboardViews
 urlpatterns = [
     path('register/', AuthViews.register, name='register'),
     path('email-verification/', AuthViews.verify_email, name='verify_email'),
-    path('login/', AuthViews.login, name='login'),
+    path('', AuthViews.login, name='login'),
+    path('forgot-password/', AuthViews.forgot_password, name='forgot_password'),
+    path('reset-password/<str:manager_id>', AuthViews.reset_password, name='reset_password'),
     path('logout/', AuthViews.logout, name='logout'),
-    path('', DashboardViews.dashboard, name='dashboard'),
+    path('dashboard/', DashboardViews.dashboard, name='dashboard'),
 
     # path('events/', DashboardViews.events, name='events'),
     # path('artists/', DashboardViews.artists, name='artists'),
--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -8,6 +8,19 @@ from apps.managers.forms import ManagerRegisterForm
 from apps.managers.models import Manager
 from apps.master.helpers.unique import *
 
+from functools import wraps
+
+# provide me decorator for authenticated view: login_required
+def login_required(view_func):
+    @wraps(view_func)
+    def wrapper(request, *args, **kwargs):
+        if 'manager_id' in request.session:
+            return view_func(request, *args, **kwargs)
+        else:
+            messages.error(request, 'You are not logged in. Please log in to access the dashboard.')
+            return redirect('login')
+    return wrapper
+
 
 class AuthViews:
     @staticmethod
@@ -106,6 +119,49 @@ class AuthViews:
                 return redirect('login')
         return render(request, 'dashboard/login.html')
 
+    def forgot_password(request):
+        if request.method == 'POST':
+            email_ = request.POST['email']
+            get_manager = Manager.objects.filter(email=email_).first()
+            if get_manager:
+                # send email with forgot password link with manager id
+                subject = 'DL91 - Forgot Password link'
+                message = f"""
+                Dear {get_manager.first_name} {get_manager.last_name},
+
+                Click on the link below to reset your password:
+                http://localhost:8000/reset-password/{str(get_manager.dl91_id)}
+                                
+                """
+                send_mail(
+                    subject,
+                    message,
+                    settings.EMAIL_HOST_USER,
+                    [f'{email_}']
+                )
+                messages.success(request, 'Check your email for link for password reset.')
+                return redirect('login')
+            else:
+                messages.error(request, 'Account not found. Please try again.')
+                return redirect('forgot_password')
+        return render(request, 'dashboard/forgot-password.html')
+
+    def reset_password(request, manager_id):
+        manager = Manager.objects.filter(dl91_id=manager_id).first()
+        if request.method == 'POST':
+            new_password_ = request.POST['new_password']
+            confirm_password_ = request.POST['confirm_password']
+            if new_password_ == confirm_password_:
+                print(new_password_, confirm_password_)
+                manager.password = make_password(new_password_)
+                manager.save()
+                messages.success(request, 'Password reset successful.')
+                return redirect('login')
+            else:
+                messages.error(request, 'Passwords do not match. Please try again.')
+                return redirect('reset_password', manager_id=manager_id)
+        return render(request, 'dashboard/reset_password.html', {'manager_id': manager_id})
+
     def logout(request):
         request.session.clear()
         messages.success(request, 'Logout successful.')
@@ -113,6 +169,7 @@ class AuthViews:
 
 
 class DashboardViews:
+    @login_required
     def dashboard(request):
         return render(request, 'dashboard/dashboard.html')
     

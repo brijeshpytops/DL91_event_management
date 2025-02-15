bfeeebcb479715e278cc42cf4f0ac6ae06ae16c2
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2025-02-15 11:58:33 +0530

    static file setup done

--- project/settings.py
+++ project/settings.py
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jnopc7p!&zu!sw_auvhggu!z)+3fya3lrzz^1oo0#@@kouxzd3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
